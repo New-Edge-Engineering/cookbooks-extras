0a19a68f5b795b87f0e50fa6157e92d02116e085
Matthew Green <matthew@newedgeengineering.com>
2012-03-30 07:28:41 +0100

    Initial creation to support fast cgi php sites.

---
+++ php/recipes/fastcgi.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook Name:: dms
+# Recipe:: web-interface
+#
+# Copyright 2011, New Edge Engineering Ltd
+#
+# All rights reserved - Do Not Redistribute
+#
+
+require_recipe "php"
+
+template "/etc/init.d/php-fcgi" do
+  source "php-fcgi.erb"
+  group "root"
+  owner "root"
+  mode 0755
+  variables(
+    :user => node['php']['fpm_user']
+  )
+end
+
+service "php-fcgi" do
+  supports :restart => true
+  action :start
+end
