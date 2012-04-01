8afda7947b547fd1aaf3a1bfe6e43b69937083d2
Matthew Green <matthew@newedgeengineering.com>
2012-04-01 08:33:47 +0100

    orrection to readme include.

--- system/metadata.rb
+++ system/metadata.rb
@@ -2,5 +2,5 @@ maintainer       "New Edge Engineering Ltd"
 maintainer_email "matthew@newedgeengineering.com"
 license          "Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php"
 description      "Installs/Configures system"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
